beb4e8c73586e0f0004d9c1204c63181bef8f13c
Zoooey2109 <zhouyuying93@163.com>
2019-09-21 18:59:22 +0800

    Homework_2

---
+++ 02/Program 1_Personalized Hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+name = input("What's your name?")
+print('Hello,' , name,'!')
+
+
+# In[ ]:
+
+
+
+
---
+++ 02/Program 2_Celsius to Fahrenheit Converter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+C = float(input("Please input degree in Celsius here: "))
+print("Converting Celsius to Fahrenheit:", C*1.8 + 32)
+
+
+# In[ ]:
+
+
+
+
