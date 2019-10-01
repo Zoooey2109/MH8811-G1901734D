eff841315612bc7868b70fe169d17ba4ff0f25ae
Zoooey2109 <zhouyuying93@163.com>
2019-10-01 14:03:07 +0800

    Homework_3

---
+++ 03/03 Program_Interactive Homework Gallery.py
@@ -0,0 +1,43 @@
+# Define greeting to world function
+def gtw():
+    print("Hello, world!")
+
+# Define greeting to you function
+def gty():
+    name = input("What's your name?")
+    print('Hello,' , name,'!')
+
+# Define temperature transformation function
+def tt():
+    C = float(input("Please input degree in Celsius here: "))
+    print("Converting Celsius to Fahrenheit:", C*1.8 + 32)
+
+# Prompt user to choose
+print("Which one do you want to run :")
+print('\nInput 1 to run "greeting to world" function, \n\
+Input 2 to run "greeting to you" function,\n\
+Input 3 to run "temperature transformation" function,\n\
+Input 0 to exit this program.')
+
+# Run the chosen sub-program
+# and go back to the stage where user is prompted to choose
+while True:
+    n = int(input("\nPlease enter your number: "))
+    if n == 1:
+        gtw()
+        continue
+    elif n == 2:
+        gty()
+        continue
+    elif n == 3:
+        tt()
+        continue
+    elif n == 0:
+        m = input("Are you sure to exit? Please enter 'yes' or 'no': ")
+        if m == "yes":
+            exit()
+        elif m == "no":
+            continue
+    else:
+        print("Please enter the correct number!")
+        continue
